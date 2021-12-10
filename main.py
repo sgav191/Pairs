907b64b543614fa4aecfe75f1b46f7303b076e79
sgav191 <swgavin0@gmail.com>
2021-12-10 11:58:27 +0000

    Initial commit (unfinished)

---
+++ main.py
@@ -0,0 +1,37 @@
+import turtle
+import random
+screen = turtle.Screen()
+drawer = turtle.Turtle()
+screen.title("Pairs")
+screen.bgcolor("#00aeff")
+def screenclick(xcor,ycor):
+	print (xcor)
+	print (ycor)
+def coordinatesx(num):
+	xcor = (num % 4) * 50 - 100
+	return xcor
+
+def coordinatesy(num):
+	ycor = (num // 4) * 50
+	return ycor
+def drawsquare(xcor,ycor):
+	drawer.penup()
+	drawer.goto(xcor,ycor)
+	drawer.color("#ff0000")
+	drawer.begin_fill()
+	drawer.pendown()
+	for num in range(4):
+		drawer.forward(45)
+		drawer.left(90)
+	drawer.end_fill()
+def draw():
+	for num in range(16):
+		xcor = coordinatesx(num)
+		ycor = coordinatesy(num)
+		drawsquare(xcor,ycor)
+numbers = list(range(1,9))*2
+random.shuffle(numbers)
+print (numbers)
+screen.onscreenclick(screenclick)
+draw()
+turtle.done()
